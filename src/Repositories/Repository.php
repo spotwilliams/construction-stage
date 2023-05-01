77cfeca5fe9e04a5115c92d6fb5ff1d926cd087e
Ricardo <ricardo.vergara@gmail.com>
2023-05-01 08:19:52 +0300

    Updated repository contract

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -3,17 +3,18 @@
 namespace ConstructionStages\Repositories;
 
 use ConstructionStages\DataTransfers\ConstructionStagesCreate;
+use ConstructionStages\DataTransfers\ConstructionStagesUpdate;
 use ConstructionStages\Models\Model;
 
 interface Repository
 {
     public function getAll(): array;
 
-    public function getSingle(int $id): array;
+    public function getSingle(int $id, bool $throwNotFound = false): ?Model;
 
-    public function store(ConstructionStagesCreate $data): array;
+    public function store(ConstructionStagesCreate $data): Model;
 
-    public function update(Model $model): array;
+    public function update(Model $model, ConstructionStagesUpdate $data): Model;
 
     public function getModelClass(): string;
 }
